40ca40cbfbc39cb646e61041a0d47626b046deb3
daria <daria_kazimierska@epam.com>
2019-12-12 09:03:23 +0100

    Add tests for DoubleNumber class

---
+++ src/test/java/pl/patrycja/game/doubleValues/DoubleNumberTest.java
@@ -0,0 +1,36 @@
+package pl.patrycja.game.doubleValues;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class DoubleNumberTest {
+
+    @DataProvider
+    public static Object[][] forTestCompare() {
+        return new Object[][] {
+                {5, 10, -1}, {-5, 10, -1}, {-10, -3, -1},
+                {0, 0, 0}, {5, 5, 0}, {-5, -5, 0},
+                {10, 5, 1}, {10, -5, 1}, {-5, -10, 1}
+        };
+    }
+
+    @Test(dataProvider = "forTestCompare")
+    public void testCompare(Number o1, Number o2, int result) {
+        DoubleNumber number = new DoubleNumber(1);
+        assertEquals(number.compare(o1, o2), result);
+    }
+
+    @DataProvider
+    public static Object[][] forTestToString() {
+        return new Object[][] {
+                {5.0, "5.0"}, {5.01, "5.01"}, {5.12222, "5.12222"}
+        };
+    }
+
+    @Test(dataProvider = "forTestToString")
+    public void testTestToString(double number, String string) {
+        assertEquals(string, new DoubleNumber(number).toString());
+    }
+}
