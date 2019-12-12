e5d4650c6aa06235a23206decd389578b7769d0e
daria <daria_kazimierska@epam.com>
2019-12-12 13:33:03 +0100

    Remove Game class
    As the result of previous updates, Game class was left with only one
    method "play". This method was moved to the main class. This simplified
    project and increased test coverage

--- src/main/java/pl/patrycja/game/main/Main.java
+++ src/main/java/pl/patrycja/game/main/Main.java
@@ -1,14 +1,26 @@
 package pl.patrycja.game.main;
 
+import pl.patrycja.game.baseValues.BaseNumber;
 import pl.patrycja.game.baseValues.BaseRange;
 import pl.patrycja.game.integerValues.IntegerRange;
+
+import static pl.patrycja.game.main.ComparisonResult.Correct;
 
 public class Main {
 
     public static void main(String[] args) {
         BaseRange range = new IntegerRange(30, 36);
         System.out.println("Hello in my magic game. Guess the number between " + range + ". Enjoy!");
-        Game game = new Game(range);
-        game.play();
+        playTheGame(range.drawRangeNumber());
+    }
+
+    static void playTheGame(BaseNumber correctNumber) {
+        BaseNumber userGuess;
+        ComparisonResult result;
+        do {
+            userGuess = NumberSource.getNumberFromUser();
+            result = ComparisonResult.of(userGuess.compareTo(correctNumber));
+            System.out.println(result.message());
+        } while (result != Correct); //TODO: check if amount od tries was exited
     }
 }
